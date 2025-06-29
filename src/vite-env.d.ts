0ba7e41b27d6d79eae1eff14362c4bdec5283992
Ken C. Demanawa <kdemanawa@talinolabs.com>
2025-06-29 08:22:49 +0800

    add the outer element wrappers

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_APP_ENV: string
+  readonly VITE_ENABLE_LOGGING: string
+  readonly MODE: string
+  readonly DEV: boolean
+  readonly PROD: boolean
+  readonly SSR: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
